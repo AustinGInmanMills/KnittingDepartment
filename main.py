8ad89ad18011a22d50a2c44f3ddf7156018e35cf
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 04:27:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ import streamlit as st
 def page2():
     st.title("Second page")
 
-pg = st.navigation([
-    st.Page("main.py", title="First page", icon="🔥"),
-    st.Page(page2, title="Second page", icon=":material/favorite:"),
-])
-pg.run()
+
+page2.run()
