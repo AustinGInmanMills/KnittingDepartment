4ee89b2830b9d8a0d594345ad1d68bf051f1c2e5
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 00:10:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import streamlit as st
 def page1():
     placeholder = st.empty()
     with placeholder.form("Bye"):
-        if st.form_submit_button:
+    submit = st.form_submit_button
+        if submit:
             st.write("Bye")
             time.sleep(3)
             placeholder.empty()
@@ -11,7 +12,8 @@ def page1():
     
 def page2():
     placeholder = st.empty()
-    with placeholder.form("Hello"):
+    submit = st.form_submit_button
+    with submit:
         if st.form_submit_button:
             st.write("Hello")
             
