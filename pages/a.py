b9d2019b7dc289cabde5fab0f5978be330fe60a7
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 10:14:24 -0400

    Update a.py

--- pages/a.py
+++ pages/a.py
@@ -2,7 +2,9 @@ import streamlit as st
 st.write(st.query_params)
 if "name" not in st.session_state:
     st.write("Name not found")
+    name = st.query_params.names
 else:
+    name = st.session_state.name
     st.write(f"{st.session_state.name} found")
 
 
