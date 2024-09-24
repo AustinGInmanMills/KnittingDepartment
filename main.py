322273b32cdfedb38173848069a5e497de532369
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 09:21:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import streamlit as st
 name = st.text_input("Name")
 name = name
 
-st.query_params.names = name
-time.sleep(0.1)
-
 if st.button("switch"):
+    st.query_params.names = name
+    time.sleep(0.1)
     st.switch_page("pages/a.py")
