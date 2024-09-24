50f0d610bd9df5078f4ca1ac8571a71da2826b9c
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 04:24:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
 import streamlit as st
-import time
 
-
-
-placeholder = st.empty()
-
-def page1():
-    with placeholder.form("Hey"):
-        submit = st.form_submit_button("click1")
-        if submit: 
-            st.write("hey")
-            st.session_state.form1 = True
-            time.sleep(1)
-            placeholder.empty()
-            time.sleep(1)
-            page2()
-        
 def page2():
-    with placeholder.form("Bye"):
-        submit = st.form_submit_button("click2")
-        if submit: 
-            st.write("bye")
-            st.session_state.form2 = True
-            time.sleep(1)
-            placeholder.empty()
-            time.sleep(1)
-            page1()
-            
-            
-def test():
-    st.write("hello")
-            
-st.button("1", on_click=test) 
-st.button("2", on_click=page2)
+    st.title("Second page")
+
+pg = st.navigation([
+    st.Page("page1.py", title="First page", icon="🔥"),
+    st.Page(page2, title="Second page", icon=":material/favorite:"),
+])
+pg.run()
