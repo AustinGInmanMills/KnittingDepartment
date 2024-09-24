107b18bf180f96110b2ca9bda8c844b58963e483
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 04:59:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+# in main.py
 import streamlit as st
 
-def page2():
-    st.title("Second page")
-
-
-yeah = st.Page(page2)
-
-yeah.run()
+st.query_params.from_dict({"bar": "foo"})
+if st.button("switch"):
+    st.switch_page("pages/a.py")
