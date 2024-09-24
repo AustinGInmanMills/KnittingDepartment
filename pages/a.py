227de0f97cc26620442ae30baf6d1a4377f20a9c
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 08:53:43 -0400

    Update a.py

--- pages/a.py
+++ pages/a.py
@@ -1,11 +1,5 @@
 import streamlit as st
 import time
 
-time.sleep(2)
 
-if "names" not in st.query_params:
-    st.rerun()
-else:
-    st.write(st.query_params.names)
-    
-st.rerun()
+st.write(st.query_params)
