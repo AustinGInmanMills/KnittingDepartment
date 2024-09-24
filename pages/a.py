63d65f5e10dfb6de779956349ce2a8a6378bd39a
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 10:06:42 -0400

    Update a.py

--- pages/a.py
+++ pages/a.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import time
-
-from streamlit import rerun
-
-rerun()
 st.write(st.query_params)
-time.sleep(5)
-st.write(st.query_params)
+if st.query_params.values() == "Austin":
+    st.write("Austin Found!!!!")
