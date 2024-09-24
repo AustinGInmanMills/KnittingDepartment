5d00977b1b962fbc489d2333364ea9404bf21f94
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 02:22:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import time
 
-placeholder = st.empty()
+placeholder1 = st.empty()
+placeholder2 = st.empty()
 
 
 def page1():
-    with placeholder.container("Hey"):
+    with placeholder1.container():
         hey = st.button("1")
         if hey:
             st.write("Hello")
@@ -13,7 +14,7 @@ def page1():
             page2()
     
 def page2():
-    with placeholder.container("Bye"):
+    with placeholder2.container():
         bye = st.button("2")
         if bye:
             st.write("Bye")
