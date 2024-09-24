a73483403fc380aa7629e95fffd2f757bba9e4b6
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 10:15:00 -0400

    Update a.py

--- pages/a.py
+++ pages/a.py
@@ -3,6 +3,7 @@ st.write(st.query_params)
 if "name" not in st.session_state:
     st.write("Name not found")
     name = st.query_params.names
+    st.write(name)
 else:
     name = st.session_state.name
     st.write(f"{st.session_state.name} found")
