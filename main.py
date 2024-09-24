a3c4b1cc62dff27053d2261d33612c1877fcc72f
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 09:08:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import streamlit as st
 
 name = st.text_input("Name")
 name = name
-st.experimental_set_query_params(
-    name=name,
-)
+st.query_params.names = name
+
 if st.button("switch"):
     st.switch_page("pages/a.py")
