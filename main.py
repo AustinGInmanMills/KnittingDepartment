1ed9abf0680982b578a98d28dd2066b0c1aad50a
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-24 03:23:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ placeholder = st.empty()
 
 def page1():
     with placeholder.form("Hey"):
-    submit = st.form_submit_button("click1")
-    if submit: 
-        st.write("hey")
-        st.session_state.run = True
+        submit = st.form_submit_button("click1")
+        if submit: 
+            st.write("hey")
+            st.session_state.run = True
         
 def page2():
     with placeholder.form("Bye"):
-    submit = st.form_submit_button("click1")
-    if submit: 
-        st.write("hey")
-        st.session_state.run = True
+        submit = st.form_submit_button("click1")
+        if submit: 
+            st.write("hey")
+            st.session_state.run = True
 
 if not "run" in st.session_state:
     page1()
